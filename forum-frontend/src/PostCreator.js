4d5dab768a81169431516c9837ab346f1fc72da1
Joao Falcao <joao.falcao@icloud.com>
2018-08-20 22:12:47 -0300

    form de postagens organizado

--- forum-frontend/src/PostCreator.js
+++ forum-frontend/src/PostCreator.js
@@ -16,7 +16,7 @@ class PostCreator extends Component {
     console.log('Titulo: ' + this.state.titulo);
     console.log('Mensagem: ' + this.state.mensagem);
 
-    axios.post('http://localhost:3333/addPost', {titulo: this.state.titulo, mensagem: this.state.mensagem})
+    axios.post('http://localhost:3333/addPost', { titulo: this.state.titulo, mensagem: this.state.mensagem })
       .then(function (response) {
         console.log(response);
         window.location.reload();
@@ -24,7 +24,7 @@ class PostCreator extends Component {
       .catch(function (error) {
         console.log(error);
       });
-      event.preventDefault();
+    event.preventDefault();
   }
 
   changeData(event) {
@@ -38,32 +38,34 @@ class PostCreator extends Component {
     return (
       <div className="row justify-content-center">
         <div className="card w-50 m-5">
-         <div class="card-header">
-         <div className="form-row justify-content-center">
-         Postagens
-         </div>
-         </div>
-          <form onSubmit={this.createPost} method="POST">
-          <div className="form-group">
-          <div className="form-row justify-content-center mb-2">
-          Assunto :
-                <input type="text" name="titulo" value={this.state.titulo}
-              onChange={this.changeData} />
+          <div class="card-header">
+            <div className="form-row justify-content-center">
+              <span>Postagens</span>
+            </div>
           </div>
+          <div className="card-body">
+            <form onSubmit={this.createPost} method="POST">
+              <div className="form-group">
+                <div className="form-row mb-2">
+                  <label for="titulo">Assunto</label>
+                  <input type="text" className="form-control" id="titulo" name="titulo" value={this.state.titulo}
+                    onChange={this.changeData} />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="form-row mb-2">
+                  <label for="mensagem">Pergunta</label>
+                  <input type="text" className="form-control" id="mensagem" name="mensagem" value={this.state.mensagem}
+                    onChange={this.changeData} />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="form-row justify-content-center">
+                  <input type="submit" value="Postar" className="btn-danger rounded" />
+                </div>
+              </div>
+            </form>
           </div>
-          <div className="form-group">
-          <div className="form-row justify-content-center mb-2">
-            Pergunta :
-                <input type="text" name="mensagem" value={this.state.mensagem}
-              onChange={this.changeData} />
-          </div>
-          </div>
-          <div className="form-group">
-          <div className="form-row justify-content-center">
-            <input type="submit" value="Submit" className="btn-danger rounded"/>
-          </div>
-          </div>
-          </form>
         </div>
       </div>
     );
